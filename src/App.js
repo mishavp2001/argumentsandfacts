0178e3b490215b518a366cdfbe6e01fb3302f4a7
Michael Polyakov <mishavp2001@yahoo.com>
2024-03-15 15:15:20 -0700

    feat: refactor

--- src/App.js
+++ src/App.js
@@ -1,148 +1,58 @@
 import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
 import {
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  TextField,
-  View,
   Authenticator,
+  Heading
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+import { Auth } from "aws-amplify";
+import Notes from "./components/Notes"
+
+const Title = styled("h1")`
+  margin-top: 0;
+  margin-bottom: 0;
+  text-transform: uppercase;
+  color: #74b49b;
+  font-size: 24px;
+`;
 
 const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-  const [sign, setSign] = useState([]);
+ 
+  const [state, setState] = useState({ isLoggedIn: false, user: null });
+
+  const checkLoggedIn = () => {
+    Auth.currentAuthenticatedUser()
+      .then(data => {
+        const user = { username: data.username, ...data.attributes };
+        setState({ isLoggedIn: true, user });
+      })
+      .catch(error => console.log(error));
+  };
 
   useEffect(() => {
-    fetchNotes();
+    checkLoggedIn();
   }, []);
 
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
-  }
 
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
 
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await Storage.remove(name);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
-
-  function showSignIn() {
-    setSign(!sign);
-  }
-
-  return (
-
-    <div>
-      <View className="App">
-        {!sign
-        && <Button onClick={showSignIn}>Sign In</Button>
-        }
-        <View margin="3rem 0" style={{}}>
-          <Heading level={2}>Latest Statments</Heading>
-          {notes.map((note) => (
-            <Flex
-              key={note.id || note.name}
-              direction="row"
-              justifyContent="left"
-              alignItems="center"
-            >
-              <Text as="strong" fontWeight={700}>
-                {note.name}
-              </Text>
-              <Text as="span">{note.description}</Text>
-              {note.image && (
-                <Image
-                  src={note.image}
-                  alt={`visual aid for ${notes.name}`}
-                  style={{ width: 400 }}
-                />
-              )}
-              <Button variation="link" onClick={() => deleteNote(note)}>
-                Delete Statment
-              </Button>
-            </Flex>
-          ))}
-        </View>
-        <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}>
-          {({ signOut, user }) => (
-            !sign && <Heading level={1}>This platform offers unrestricted freedom of speech to everyone, free of charge! You can sign up using your IP address and engage in discussions on any topic without fear of persecution. We acknowledge that political parties and nations often manipulate news and facts to suit their agendas. Therefore, seize this opportunity to express your views and ensure your voice is heard loud and clear!</Heading>
-            && sign && <><Button onClick={signOut}>Sign Out</Button>
-              <div style={{}}>
-                <View as="form" margin="3rem 0" onSubmit={createNote}>
-                  <Flex direction="column" justifyContent="left">
-                    <TextField
-                      name="name"
-                      placeholder="Statment"
-                      label="Statment"
-                      labelHidden
-                      variation="quiet"
-                      required />
-                    <TextField
-                      name="description"
-                      placeholder="Argemnet & Facts"
-                      label="Argemnet"
-                      labelHidden
-                      variation="quiet"
-                      required />
-                    <View
-                      name="image"
-                      as="input"
-                      type="file"
-                      placeholder="Add Image"
-                      style={{}} />
-                    <Button type="submit" variation="primary">
-                      Publish Statment
-                    </Button>
-                  </Flex>
-                </View>
-              </div></>
-          )}
-        </Authenticator>
-      </View>
-    </div>
+  
+  return state.isLoggedIn ? (
+    <Notes />
+  ) : (
+    <>
+      <Title>Articles</Title>
+      <Heading level={1}>This platform offers unrestricted freedom of speech to everyone, free of charge! You can sign up using your IP address and engage in discussions on any topic without fear of persecution. We acknowledge that political parties and nations often manipulate news and facts to suit their agendas. Therefore, seize this opportunity to express your views and ensure your voice is heard loud and clear!
+      </Heading>
+      <Authenticator
+        onStateChange={authState => {
+          if (authState === "signedIn") {
+            checkLoggedIn();
+          }
+        }}
+      />
+    </>
   );
 };
 
---
+++ src/components/Notes.js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import { Auth } from "aws-amplify";
+import {
+    Button,
+    Flex,
+    Image,
+    Text,
+    TextField,
+    View,
+  } from "@aws-amplify/ui-react";
+  import { listNotes } from "../graphql/queries";
+  import {
+    createNote as createNoteMutation,
+    deleteNote as deleteNoteMutation,
+  } from "../graphql/mutations";
+  import { API } from "aws-amplify";
+
+
+const Header = styled("div")`
+  background-color: #ffffff;
+  padding-left: 16px;
+  padding-right: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  right: 0;
+  left: 0;
+  height: 80px;
+  z-index: 2;
+`;
+
+const Title = styled("h1")`
+  margin-top: 0;
+  margin-bottom: 0;
+  text-transform: uppercase;
+  color: #74b49b;
+  font-size: 24px;
+`;
+
+const SignOutButton = styled(Button)`
+  background-color: #74b49b;
+  cursor: pointer;
+`;
+
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+    setNotes(notesFromAPI);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const image = form.get("image");
+    const data = {
+      name: form.get("name"),
+      description: form.get("description"),
+      image: image.name,
+    };
+    if (!!data.image) await Storage.put(data.name, image);
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: data },
+    });
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id, name }) {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <>
+      <Header>
+        <Title>Articles</Title>
+        <SignOutButton
+          onClick={() => {
+            Auth.signOut().then(() => window.location.reload());
+          }}
+        >
+          Sign Out
+        </SignOutButton>
+      </Header>
+      <View className="App">
+  <View margin="3rem 0" style={{}}>
+    {notes.map((note) => (
+      <Flex
+        key={note.id || note.name}
+        direction="row"
+        justifyContent="left"
+        alignItems="center"
+      >
+        <Text as="strong" fontWeight={700}>
+          {note.name}
+        </Text>
+        <Text as="span">{note.description}</Text>
+        {note.image && (
+          <Image
+            src={note.image}
+            alt={`visual aid for ${notes.name}`}
+            style={{ width: 400 }}
+          />
+        )}
+        <Button variation="link" onClick={() => deleteNote(note)}>
+          Delete Statment
+        </Button>
+      </Flex>
+    ))}
+  </View>
+
+        <div style={{}}>
+          <View as="form" margin="3rem 0" onSubmit={createNote}>
+            <Flex direction="column" justifyContent="left">
+              <TextField
+                name="name"
+                placeholder="Statment"
+                label="Statment"
+                labelHidden
+                variation="quiet"
+                required />
+              <TextField
+                name="description"
+                placeholder="Argemnet & Facts"
+                label="Argemnet"
+                labelHidden
+                variation="quiet"
+                required />
+              <View
+                name="image"
+                as="input"
+                type="file"
+                placeholder="Add Image"
+                style={{}} />
+              <Button type="submit" variation="primary">
+                Publish Statment
+              </Button>
+            </Flex>
+          </View>
+        </div>
+</View>
+    </>
+  );
+};
+
+export default Notes;
