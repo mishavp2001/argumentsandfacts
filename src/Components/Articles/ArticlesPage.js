711ad48421057293d651248032fe03a2ef068165
Michael Polyakov <mishavp2001@yahoo.com>
2024-03-31 16:55:08 -0700

    feat: add chat debate

--- src/Components/Articles/ArticlesPage.js
+++ src/Components/Articles/ArticlesPage.js
@@ -28,9 +28,9 @@ const ArticlesPage = () => {
   const { route, user, signOut } = useAuthenticator((context) => [
     context.route,
     context.signOut,
-]);
+  ]);
 
-//console.log(user?.username);
+  //console.log(user?.username);
 
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
@@ -70,16 +70,36 @@ const ArticlesPage = () => {
 
   async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
-    try{
+    try {
       await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id }},
-      authMode: user?.username ? 'AMAZON_COGNITO_USER_POOLS' : 'AWS_IAM'
+        query: deleteNoteMutation,
+        variables: { input: { id } },
+        authMode: user?.username ? 'AMAZON_COGNITO_USER_POOLS' : 'AWS_IAM'
       });
       await Storage.remove(name);
       setNotes(newNotes);
     } catch {
-      setAlertActive(true); 
+      setAlertActive(true);
+    }
+  }
+
+  async function debateNote(note) {
+    //const newNotes = notes.filter((note) => note.id !== id);
+    try {
+      const resp = await fetch("https://9xsl4q3gdk.execute-api.us-east-2.amazonaws.com/Prod/getPrompt", {
+        method: "POST",
+        body: JSON.stringify({
+          data: 'Debate:' +  note.name }
+        ),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS    
+        }
+      });
+      alert(resp);
+    } catch {
+      setAlertActive(true);
     }
   }
 
@@ -91,52 +111,57 @@ const ArticlesPage = () => {
 
     <Container fluid className='px-3'>
       {alertActive && (
-        <Alert 
-          colorTheme="warning" 
+        <Alert
+          colorTheme="warning"
           isDismissible={true}
-          onDismiss={() => setAlertActive(false)}  
+          onDismiss={() => setAlertActive(false)}
         >
-              You don't have permission
-          </Alert>)
-        }
+          You don't have permission
+        </Alert>)
+      }
       <Row>
         <Col className='context-col'>
-            <View className="articles">
-              <View>
-                {notes.map((note) => (
-                  <Flex
-                    className="articles-list"
-                    key={note.id || note.name}
-                    direction="row"
-                    justifyContent="left"
-                    alignItems="center"
-                  >
-                    <Text as="strong" className="f1" fontWeight={700}>
-                      {note.name}
-                    </Text>
-                    <Text className="f2" as="span">{note.description}</Text>
-                    {note.image && (
-                      <Image
-                        className="fs"
-                        src={note.image}
-                        alt={`visual aid for ${notes.name}`}
-                      />
-                    )}
+          <View className="articles">
+            <View>
+              {notes.map((note) => (
+                <Flex
+                  className="articles-list"
+                  key={note.id || note.name}
+                  direction="row"
+                  justifyContent="left"
+                  alignItems="center"
+                >
+                  <Text as="strong" className="f1" fontWeight={700}>
+                    {note.name}
+                  </Text>
+                  <Text className="f2" as="span">{note.description}</Text>
+                  {note.image && (
+                    <Image
+                      className="fs"
+                      src={note.image}
+                      alt={`visual aid for ${notes.name}`}
+                    />
+                  )}
 
-                    {route === 'authenticated' && (
+                  {route === 'authenticated' && (
+                    <div>
                       <Button variation="link" onClick={() => deleteNote(note)}>
-                      Delete
-                    </Button>
-                    )}
-                  </Flex>
-                ))}
-              </View>
+                        Delete
+                      </Button>
+                      <Button variation="link" onClick={() => debateNote(note)}>
+                        Debate
+                      </Button>
+                    </div>
+                  )}
+                </Flex>
+              ))}
+            </View>
 
-              {user?.username &&
-                <div className="article-publish">
+            {user?.username &&
+              <div className="article-publish">
                 <View as="form" margin="3rem 0" onSubmit={createNote}>
                   <Flex direction="column" justifyContent="left">
-                    <span>Publish your article</span>    
+                    <span>Publish your article</span>
                     <TextField
                       name="name"
                       placeholder="Statment"
@@ -163,7 +188,7 @@ const ArticlesPage = () => {
                   </Flex>
                 </View>
               </div>}
-            </View>
+          </View>
         </Col>
       </Row>
     </Container>
